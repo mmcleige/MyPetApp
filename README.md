4204563eee5ab293d23ee2e9f403175d6413b0f1
pjh <3150420650@qq.com>
2026-04-01 16:30:59 +0800

    [0.2.0]初步创建底部导航栏

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 彭佳豪22210129
 个人毕业设计
 宠物健康监测app
-Android Studio LadyBug
+Android Studio LadyBug android-studio-2024.2.2.13-windows
 远端同步成功
 双向同步成功
--- app/src/main/java/com/mmcleige/petapplication/MainActivity.kt
+++ app/src/main/java/com/mmcleige/petapplication/MainActivity.kt
@@ -5,13 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mmcleige.petapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // 1. 声明一个 binding 变量（你的智能界面管家）
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // 2. 唤醒管家，让他把 activity_main.xml 里的东西全转换成代码能认识的对象
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // 3. 把管家手里的根视图（root）展示到手机屏幕上
+        setContentView(binding.root)
+
+        // 4. 让界面适配全面屏（这里用 binding.root 完美替代了原来的 R.id.main）
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
